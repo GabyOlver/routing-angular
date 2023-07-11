caf54b390793f6ce68c0b4ab3f59a4864f40e95c
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-07-10 19:24:19 -0600

    rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,53 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component'
+import { MenuComponent } from './menu/menu.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 //Login
 //Menu
 //Pedidos
 
+const routes: Routes = [
+{
+  path: 'login',
+  component: LoginComponent,
+},
+{
+  path: 'menu',
+  component: MenuComponent,
+},
+{
+  path: 'pedidos',
+  component: PedidosComponent,
+},
+{
+  path: '',
+  redirectTo: '/login',
+  pathMatch: "full"
+},
+{
+  path: '**',
+  component: NotFoundComponent,
+}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    PedidosComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+    //forRoot especifica que lo que tenga dentro es el enrutador principal
   ],
   providers: [],
   bootstrap: [AppComponent]
